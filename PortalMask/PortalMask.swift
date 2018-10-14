ded49d30ee1d8d31bdec9a3b76b170160496d982
Maxx Cobb <maxx.cobb@blippar.com>
2018-10-14 09:18:50 -0700

    added documentation and moved some code that could be reused to separate functions

--- PortalMask/PortalMask.swift
+++ PortalMask/PortalMask.swift
@@ -8,41 +8,33 @@
 
 import SceneKit.SCNNode
 
+// MARK: -
+/// A node containing a geometry that will create a portal of the specified properties and dimensions.
 open class PortalMask: SCNNode {
-	private let geometryHolder = SCNNode()
-	private var bezierPath = UIBezierPath()
-	private func addOutsideBox(boxSize: CGFloat) {
-		self.bezierPath.move(to: CGPoint(x: -boxSize, y: -boxSize))
-		self.bezierPath.addLine(to: CGPoint(x: -boxSize, y: boxSize))
-		self.bezierPath.addLine(to: CGPoint(x: boxSize, y: boxSize))
-		self.bezierPath.addLine(to: CGPoint(x: boxSize, y: -boxSize))
-		self.bezierPath.close()
-	}
+	/**
+	Initializes a Portal of a given width and height.
+
+	- Parameters:
+		- frameSize: [CGSize](apple-reference-documentation://hsWG5FZjhU) of the width and height the portal should be
+		- depth: Optional depth the portal should go back. If no value is given it will default to twice the largest dimention of the frame
+		- outerMult: Optional size the mask around the frame should be. The default is three times the largest dimention of the frame.
+
+	- Returns: A new rectangular portal
+	*/
 	public init(frameSize: CGSize, depth: CGFloat! = nil, outerMult: CGFloat = 3) {
 		super.init()
 		let depth = depth ?? max(frameSize.width, frameSize.height) * 2
-		bezierPath.usesEvenOddFillRule = true
-		let hiderSize = max(frameSize.width, frameSize.height) * max(1,outerMult)
-		self.addOutsideBox(boxSize: hiderSize)
+		self.bezierPath.usesEvenOddFillRule = true
 
+		let hiderSize = max(frameSize.width, frameSize.height) * max(1,outerMult)
+		self.bezierPath.addFrame(frame: CGRect(origin: CGPoint.zero, size: CGSize(width: hiderSize, height: hiderSize)))
+
+		// dividing by exactly 2 doesn't work so well for tracking images
 		let innerW = frameSize.width / 1.975
 		let innerH = frameSize.height / 1.975
-		bezierPath.move(to: CGPoint(x: -innerW, y: -innerH))
-		bezierPath.addLine(to: CGPoint(x: -innerW, y: innerH))
-		bezierPath.addLine(to: CGPoint(x: innerW, y: innerH))
-		bezierPath.addLine(to: CGPoint(x: innerW, y: -innerH))
-		bezierPath.close()
+		self.bezierPath.addFrame(frame: CGRect(origin: CGPoint.zero, size: CGSize(width: innerW, height: innerH)))
 
-		let mask = SCNMaterial()
-		mask.colorBufferWriteMask = SCNColorMask(rawValue: 0)
-		mask.isDoubleSided = true
-		let seeInside = SCNMaterial()
-		seeInside.diffuse.contents = UIColor.clear
-
-		let shape = SCNShape(path: bezierPath, extrusionDepth: depth)
-		shape.materials = [mask, SCNMaterial(), seeInside]
-
-		self.geometryHolder.geometry = shape
+		self.geometryHolder.geometry = self.maskSCNShape(path: self.bezierPath, extrusionDepth: depth)
 		self.geometryHolder.position.z = Float(-depth / 2) + 0.0003
 		self.addChildNode(self.geometryHolder)
 	}
@@ -51,4 +43,21 @@ open class PortalMask: SCNNode {
 		fatalError("init(coder:) has not been implemented")
 	}
 
+	private let geometryHolder = SCNNode()
+	private var bezierPath = UIBezierPath()
+
+	private func materialList() -> [SCNMaterial] {
+		let mask = SCNMaterial()
+		mask.colorBufferWriteMask = SCNColorMask(rawValue: 0)
+		mask.isDoubleSided = true
+		let seeInside = SCNMaterial()
+		seeInside.diffuse.contents = UIColor.clear
+		return [mask, SCNMaterial(), seeInside]
+	}
+
+	private func maskSCNShape(path: UIBezierPath, extrusionDepth: CGFloat) -> SCNShape {
+		let shape = SCNShape(path: self.bezierPath, extrusionDepth: extrusionDepth)
+		shape.materials = self.materialList()
+		return shape
+	}
 }
---
+++ PortalMask/UIBezierPath+Extensions.swift
@@ -0,0 +1,23 @@
+//
+//  UIBezierPath+Extensions.swift
+//  PortalMask
+//
+//  Created by Max Cobb on 14/10/2018.
+//  Copyright © 2018 Max Cobb. All rights reserved.
+//
+
+import UIKit
+
+private func +(left: CGPoint, right: CGPoint) -> CGPoint {
+	return CGPoint(x: left.x + right.x, y: left.y + right.y)
+}
+
+internal extension UIBezierPath {
+	func addFrame(frame: CGRect) {
+		self.move(to: CGPoint(x: -frame.width, y: -frame.height) + frame.origin)
+		self.addLine(to: CGPoint(x: -frame.width, y: frame.height) + frame.origin)
+		self.addLine(to: CGPoint(x: frame.width, y: frame.height) + frame.origin)
+		self.addLine(to: CGPoint(x: frame.width, y: -frame.height) + frame.origin)
+		self.close()
+	}
+}
